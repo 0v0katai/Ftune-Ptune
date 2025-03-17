64fb623b781b9b5b43b0bacaffe9beb99972fb46
CalcLoverHK <0v0katai@gmail.com>
2025-03-17 13:23:53 +0800

    feat: add commit ID display

--- Ptune4/src/settings.c
+++ Ptune4/src/settings.c
@@ -30,7 +30,7 @@ enum select_option
 
 static void about()
 {
-    row_print(10, 1, "Ptune4 v0.04");
+    row_print(10, 1, VERSION);
     row_print(11, 1, "Copyright (C) 2025 Sentaro21, CalcLoverHK");
     row_print(12, 1, "This software is licensed under MIT/X11.");
 }
