06a911180c7bdfb4c15d77dfb0f6c1220457ff8c
CalcLoverHK <0v0katai@gmail.com>
2025-04-11 15:57:05 +0800

    feat: global screenshot function

--- Ptune4/src/main.c
+++ Ptune4/src/main.c
@@ -2,10 +2,32 @@
 #include <gint/gint.h>
 #include <gint/clock.h>
 #include <gint/gdb.h>
+#include <gint/keyboard.h>
+#include <gint/usb.h>
+#include <gint/usb-ff-bulk.h>
 #include <libprof.h>
 
 #include "config.h"
 #include "menu.h"
+
+static bool global_getkey(key_event_t key)
+{
+    #if defined ENABLE_USB
+    if (key.shift && (key.key == KEY_EXP || key.key == KEY_7))
+    {
+        if (usb_is_open())
+            usb_fxlink_screenshot(true);
+        else
+        {
+            usb_interface_t const *interfaces[] = {&usb_ff_bulk, NULL};
+            usb_open(interfaces, GINT_CALL_NULL);
+            usb_open_wait();
+        }
+        return true;
+    }
+    #endif
+    return false;
+}
 
 int main()
 {
@@ -21,6 +43,7 @@ int main()
     prof_init();
     cpg_set_overclock_permanent(true);
     gint_setrestart(true);
+    getkey_set_feature_function(global_getkey);
 
     express_menu();
     return 1;
