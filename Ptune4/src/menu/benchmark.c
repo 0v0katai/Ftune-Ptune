6ed30c6df2d3235f3cad568d6edc3f99f37650a2
CalcLoverHK <0v0katai@gmail.com>
2025-05-25 01:38:24 +0800

    feat: ETMU-based dupdate validation

--- Ptune4/src/menu/benchmark.c
+++ Ptune4/src/menu/benchmark.c
@@ -9,7 +9,33 @@
 #include "menu.h"
 
 #ifdef ENABLE_AZUR
-#include <azur/gint/render.h>
+# include <azur/gint/render.h>
+#endif
+
+#if defined CG50 || defined CG100 || defined CP400
+# define RAM_ADDRESS 0xAC150000
+#else
+# define RAM_ADDRESS 0xA8150000
+#endif
+
+#if defined CP400
+# define SCORE_X(i) 2
+# define SCORE_ROW(i) (19 + (i))
+# define UPDATE_ROW 23
+# define DHRYSTONE_ROW 24
+# define WHETSTONE_X 2
+# if defined ENABLE_WHETSTONE
+#  define WHETSTONE_ROW 25
+# else
+#  define WHETSTONE_ROW 24
+# endif
+#else
+# define SCORE_X(i) ((i) * 12 + 2)
+# define SCORE_ROW(i) 11
+# define UPDATE_ROW 12
+# define DHRYSTONE_ROW 13
+# define WHETSTONE_X 26
+# define WHETSTONE_ROW 13
 #endif
 
 u32 tick_count(volatile bool *flag);
@@ -31,11 +57,7 @@ void run_benchmark()
     benchmark_data[0] = tick_count(&bench_flag) / 100;
     for (int i = 0; i < 3; i++)
     {
-        #if defined CG50 || defined CG100 || defined CP400
-        static const u32 address[] = {0xa0150000, 0xac150000, 0xa4150000};
-        #else
-        static const u32 address[] = {0xa0150000, 0xa8150000, 0xa4150000};
-        #endif
+        static const u32 address[] = {0xA0150000, RAM_ADDRESS, 0xA4150000};
         bench_flag = true;
         timer_start(timer_configure(TIMER_TMU, 50000, GINT_CALL(disable_bench_flag)));
         benchmark_data[i + 1] = mem_bench((u32 *)address[i], &bench_flag);
@@ -44,7 +66,12 @@ void run_benchmark()
     #ifdef ENABLE_AZUR
     benchmark_data[5] = prof_exec(azrp_update());
     #endif
+    int ETMU_timer = timer_configure(TIMER_ETMU, 0xFFFFFFFF, GINT_CALL_NULL);
+    timer_start(ETMU_timer);
+    int ETMU_start = SH7305_ETMU[ETMU_timer - 3].TCNT;
     benchmark_data[4] = prof_exec(dupdate());
+    u32 ETMU_time = 32768 / (ETMU_start - SH7305_ETMU[ETMU_timer - 3].TCNT);
+    timer_stop(ETMU_timer);
 
     #ifdef ENABLE_DHRY
     benchmark_data[6] = prof_exec(dhrystone(DHRY_LOOP));
@@ -54,46 +81,26 @@ void run_benchmark()
     benchmark_data[7] = prof_exec(whetstone());
     #endif
     
-    #if defined CP400
     for (int i = 0; i < 4; i++)
     {
         static const char *score_name[] = {"CPU", "ROM", "RAM", "I/O"};
-        row_print(19 + i, 2, "%s %d", score_name[i], benchmark_data[i]);
+        row_print(SCORE_ROW(i), SCORE_X(i), "%s %d", score_name[i], benchmark_data[i]);
     }
-    row_print(23, 2, "dupdate: %d us/%d FPS", benchmark_data[4], 1000000 / benchmark_data[4]);
+    row_print_color(UPDATE_ROW, 2, ETMU_time < (1000000 / benchmark_data[4]) ? C_RGB(0,31,31) : C_RGB(31,0,31), C_WHITE,
+        "dupd: %d us/%d (%d) FPS", benchmark_data[4], 1000000 / benchmark_data[4], ETMU_time);
     
-    # ifdef ENABLE_DHRY
-    row_print(24, 2, "INT: %llu Dhrystone/s", DHRY_LOOP * 1000000ull / benchmark_data[6]);
-    # endif
-
-    # ifdef ENABLE_WHET
-    row_print(25, 2, "DBL: %d KWIPS", 100 * ITERATIONS * 1000000 / benchmark_data[7]);
-    row_highlight(25);
-    # endif
-
-    for (int i = 0; i < 6; i++)
-        row_highlight(19 + i);
-    #else
-    for (int i = 0; i < 4; i++)
-    {
-        static const char *score_name[] = {"CPU", "ROM", "RAM", "I/O"};
-        row_print(11, 2 + i * 12, "%s %d", score_name[i], benchmark_data[i]);
-    }
-
-    row_print(12, 2, "dupd: %d us/%d FPS", benchmark_data[4], 1000000 / benchmark_data[4]);
-    # ifdef ENABLE_AZUR
-    row_print(12, 26, "azrp: %d us/%d FPS", benchmark_data[5], 1000000 / benchmark_data[5]);
-    # endif
-
-    # ifdef ENABLE_DHRY
-    row_print(13, 2, "INT: %llu Dhrystone/s", DHRY_LOOP * 1000000ull / benchmark_data[6]);
-    # endif
-
-    # ifdef ENABLE_WHET
-    row_print(13, 26, "DBL: %d KWIPS", 100 * ITERATIONS * 1000000 / benchmark_data[7]);
-    # endif
-
-    for (int i = 0; i < 3; i++)
-        row_highlight(11 + i);
+    #ifdef ENABLE_AZUR
+    row_print(UPDATE_ROW, 26, "azrp: %d us/%d FPS", benchmark_data[5], 1000000 / benchmark_data[5]);
     #endif
+
+    #ifdef ENABLE_DHRY
+    row_print(DHRYSTONE_ROW, 2, "INT: %llu Dhrystone/s", DHRY_LOOP * 1000000ull / benchmark_data[6]);
+    #endif
+
+    #ifdef ENABLE_WHET
+    row_print(WHETSTONE_ROW, WHETSTONE_X, "DBL: %d KWIPS", 100 * ITERATIONS * 1000000 / benchmark_data[7]);
+    #endif
+
+    for (int i = SCORE_ROW(0); i != WHETSTONE_ROW + 1; i++)
+        row_highlight(i);
 }
